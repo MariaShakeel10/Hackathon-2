fd9792d6ed4aaa57e20b9d7c1d886303f3bcb8a1
MariaShakeel10 <mariashakeel10a@gmail.com>
2025-01-03 14:14:04 +0500

    1st

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBatat2rws4Ly_GosMHp1NGHBOBXTD2CwA",
+  authDomain: "hackathon-2bec4.firebaseapp.com",
+  projectId: "hackathon-2bec4",
+  storageBucket: "hackathon-2bec4.firebasestorage.app",
+  messagingSenderId: "717047087225",
+  appId: "1:717047087225:web:910338c4861f0a814714a8"
+};
